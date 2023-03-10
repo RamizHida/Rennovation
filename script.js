118f0ef1ae8b8bc42287317737abb8e0a22b433f
Ramiz Hida <ramizhida00@gmail.com>
2023-03-10 20:25:39 +0100

    Added dropdown-menu functionality

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 let dropDown = document.querySelector('.menu-icon');
+let menu = document.querySelector('.heading');
+let headerContainer = document.querySelector('.heading--container');
+let expand = true;
+dropDown.addEventListener('click', () => {
+  if (expand) {
+    expand = false;
+    menu.classList.remove('collapsed');
+  } else {
+    menu.classList.add('collapsed');
+    menu.removeAttribute('collapsed');
+    expand = true;
+  }
+});
